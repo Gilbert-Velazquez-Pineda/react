c2b6d74ac1d0a8e30f5f61c04db12e3b95142eaf
gilbertovelazquez <gnilberto_1@hotmail.com>
2022-09-01 19:05:08 -0500

    inicio git

--- src/App.js
+++ src/App.js
@@ -5,14 +5,24 @@ import Inicio from './componentes/paginas/Inicio'
 import Prueba from './componentes/paginas/Prueba'
 import Test from './componentes/paginas/Test'
 import ItemListContainer from './componentes/navbar/ItemListContainer'
+import { useState } from 'react';
+
 
 
 function App() {
+
+  const [counter, setCounter] = useState (5)
+
 
   return (
     <div className="App">
       <Router>
           <NavBar/>
+
+          <div className='my-2'>
+          <strong>Contador: {counter}</strong>
+          </div>
+
           <ItemListContainer/>
           <Routes>
             <Route exact path="/" element={<Inicio/>}/>
